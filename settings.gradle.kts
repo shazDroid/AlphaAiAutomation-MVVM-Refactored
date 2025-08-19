77d7cd91335d22c1a2d180fcd90506c2477474c9
shazDroid <shahbazansari52@gmail.com>
2025-08-19 09:41:35 -0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,12 @@ pluginManagement {
         id("org.jetbrains.compose").version(extra["compose.version"] as String)
         id("org.jetbrains.kotlin.plugin.compose").version(extra["kotlin.version"] as String)
     }
+
 }
 
 rootProject.name = "Alpha AI Automation"
+
+include(":modules:adb")
+include(":modules:domain")
+include(":modules:extractor")
+include(":modules:app")
